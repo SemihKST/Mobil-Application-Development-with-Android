b6c80a1179238dea570e9e46de3192167cf38cf6
SemihKST <semihkst@gmail.com>
2024-11-28 16:32:49 +0300

    Mobil Application Development with Android

---
+++ URL_Shorten/app/src/main/java/com/semih/urlshorten/ShortenResponse.java
@@ -0,0 +1,14 @@
+package com.semih.urlshorten;
+
+public class ShortenResponse {
+
+    private String link;
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
